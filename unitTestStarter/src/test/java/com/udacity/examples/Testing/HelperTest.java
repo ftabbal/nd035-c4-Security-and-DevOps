1d4d9ccaa1a840e5580b1152a589afbe5e8ad6f7
Fred <fred@home.local>
2021-01-07 16:43:58 -0500

    Implement some tests for practice following the course.

--- unitTestStarter/src/test/java/com/udacity/examples/Testing/HelperTest.java
+++ unitTestStarter/src/test/java/com/udacity/examples/Testing/HelperTest.java
@@ -1,8 +1,36 @@
 package com.udacity.examples.Testing;
 
-import junit.framework.TestCase;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.IntSummaryStatistics;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 
 public class HelperTest {
 
-	
+    @Test
+    public void verify_getCount_noEmptyEntries() {
+        List<String> stringList = Arrays.asList("Testing", "this", "method");
+        long actual = Helper.getCount(stringList);
+        assertEquals(3, actual);
+    }
+
+    @Test
+    public void verify_getStats() {
+        List<Integer> yrsOfExperience = Arrays.asList(13,4,15,6,17,8,19,1,2,3);
+        IntSummaryStatistics stats = Helper.getStats(yrsOfExperience);
+        assertEquals(19, stats.getMax());
+    }
+
+    @Test
+    public void verify_getMergedList() {
+        List<String> stringList = Arrays.asList("Testing", "this", "method");
+        String actual = Helper.getMergedList(stringList);
+        String expected = "Testing, this, method";
+        assertEquals(expected, actual);
+
+    }
 }
