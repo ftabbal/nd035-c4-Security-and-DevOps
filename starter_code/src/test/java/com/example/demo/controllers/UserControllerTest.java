c4178c28a12b2451ba93e1b759925b2dcf249b23
Fred <fred@home.local>
2021-01-08 17:57:39 -0500

    Add tests for UserController

---
+++ starter_code/src/test/java/com/example/demo/controllers/UserControllerTest.java
@@ -0,0 +1,94 @@
+package com.example.demo.controllers;
+
+import com.example.demo.model.persistence.Cart;
+import com.example.demo.model.persistence.User;
+import com.example.demo.model.requests.CreateUserRequest;
+import com.example.demo.services.UserService;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class UserControllerTest {
+
+    private UserController userController;
+    private final UserService userService = mock(UserService.class);
+    private final BCryptPasswordEncoder encoder = mock(BCryptPasswordEncoder.class);
+
+    @Before
+    public void setUp() {
+        userController = new UserController(userService, encoder);
+    }
+
+    @Test
+    public void verify_createUser_creationSuccess() {
+        User expectedUser = createUser();
+        when(userService.createUser(any(User.class))).thenReturn(expectedUser);
+        when(encoder.encode("testPassword")).thenReturn("thisIsHashed");
+        CreateUserRequest request = new CreateUserRequest();
+        request.setUsername("test");
+        request.setPassword("testPassword");
+        request.setConfirmPassword("testPassword");
+
+        ResponseEntity<User> response = userController.createUser(request);
+        assertNotNull(response);
+        assertEquals(200, response.getStatusCodeValue());
+
+        User user = response.getBody();
+        assertNotNull(user);
+        assertEquals(0, user.getId());
+        assertEquals("test", user.getUsername());
+        assertEquals("thisIsHashed", user.getPassword());
+        assertNotNull(user.getCart());
+    }
+
+    @Test
+    public void verify_createUser_nonMatchingPassword() {
+        CreateUserRequest request = new CreateUserRequest();
+        request.setUsername("test");
+        request.setPassword("testPassword");
+        request.setConfirmPassword("invalidPassword");
+
+        ResponseEntity<User> response = userController.createUser(request);
+        assertNotNull(response);
+        assertEquals(400, response.getStatusCodeValue());
+    }
+
+    @Test
+    public void verify_findById_userExists() {
+        User expectedUser = createUser();
+        when(userService.getUserById(any(Long.class))).thenReturn(expectedUser);
+
+        ResponseEntity<User> response = userController.findById(0L);
+        assertNotNull(response);
+        assertEquals(200, response.getStatusCodeValue());
+        assertNotNull(response.getBody());
+    }
+
+    @Test
+    public void verify_findByName_userExists() {
+        User expectedUser = createUser();
+        when(userService.getUserByName(any(String.class))).thenReturn(expectedUser);
+
+        ResponseEntity<User> response = userController.findByUserName("test");
+        assertNotNull(response);
+        assertEquals(200, response.getStatusCodeValue());
+        assertNotNull(response.getBody());
+    }
+
+    private User createUser() {
+        User user = new User();
+        user.setPassword("thisIsHashed");
+        user.setId(0);
+        user.setUsername("test");
+        user.setCart(new Cart());
+        return user;
+    }
+
+}
